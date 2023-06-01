015e9fccf2dc95de65d1f5ddcef94ed692c3341e
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-01 11:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const value=document.getElementById("browser-info");
+value.innerText=`You are using " + ${navigator.userAgent} + " ${navigator.appVersion} " + version`
